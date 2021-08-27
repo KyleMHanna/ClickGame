405e0f673e349cb1bb3d7d64862b491b737ad520
Kyle Hanna <hanna.kyle@live.com>
2021-08-27 14:22:10 -0600

    layout and wireframe done

---
+++ main.js
@@ -0,0 +1,25 @@
+let clickUpgrades = {
+    pickaxes: {
+      price: 200,
+      quantity: 0,
+      multiplier: 1
+    },
+    cheeseknife: {
+        price: 100,
+        quantity: 0,
+        multiplier: 1
+      }
+  };
+  
+  let automaticUpgrades = {
+    helper1: {
+      price: 600,
+      quantity: 0,
+      multiplier: 2
+    },
+    helper2: {
+        price: 600,
+        quantity: 0,
+        multiplier: 5
+  };
+
