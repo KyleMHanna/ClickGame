a286c13adce3430ce74e7791e152fba396c1e7bf
Kyle Hanna <hanna.kyle@live.com>
2021-08-27 15:52:08 -0600

    inventory counter done

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 let currency =0;
-var item1 = 0;
-var item2 = 0;
+var item1 =0;
+var item2 =0;
 let item3 =0;
 
 var clicks =0;
@@ -9,10 +9,19 @@ var clicks =0;
 // clicker count function
 function onClick(){
 clicks += 1;
-document.getElementById('clicks').innerHTML = clicks;
+document.getElementById("clicks").innerHTML = clicks;
 };
 
-//function addItem1()[
- //   item1 += 1;
-///   document.getElementById("addItem1").innerHTML = item1;
-//]//;
+function addItem1(){
+  item1 += 1;
+ document.getElementById("addItem1").innerHTML = item1;
+};
+
+function addItem2(){
+    item2 += 1;
+   document.getElementById("addItem2").innerHTML = item2;
+  };
+function addItem3(){
+    item3 += 1;
+   document.getElementById("addItem3").innerHTML = item3;
+  };
